ac925ce474cbe0ffacabfe7c5bedbeb8968550e5
Jesse Caramalis <101160675+jesseCaramalis@users.noreply.github.com>
2023-03-13 12:34:37 +1000

    updated readme

--- README.md
+++ README.md
@@ -3,8 +3,15 @@ This is a React Native forum application.
 
 ## To Do Features
 
+- Search bar
 - Posting
 - Commenting (add/remove)
 - Likes
 - Auth Register/Login modal
 - T&C's
+
+## Optimizations
+
+- Fix sensitive touchable post components on feed screen
+- Extensively test comments/comment nesting within parameters of API.
+- Better abstraction with styles, parts of FeedScreen, and types.
--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -1,3 +1,6 @@
+// this utility function accepts a date string from the API and returns an array of strings containing the day, month and year, respectively.
+// it is used in the Post and Comment components to display the date of the post in the desired format.
+
 const parseDate = (dateString: string): [string, string, string] => {
     const date = new Date(dateString);
     const day = date.getDate().toString().padStart(2, '0');
